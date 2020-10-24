c666eceef30e947b7cb54ea54ff3a9e9d0f3995b
“Lavinia <laviniateodorescu@college.harvard.edu>
2020-10-24 22:13:52 +0300

    Graph 2 + Page 2

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -9,6 +9,8 @@
 
 
 x <- readRDS(file = "data/ILO_LFP.rds")
+post_comm_list <- readRDS(file = "data/post_comm_list.rds")
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -16,15 +18,67 @@ shinyServer(function(input, output) {
     output$senatePlot <- renderPlot({
 
     
-        x %>%
-            rename("female_labor" = `Labor force participation rate, female (% of female population ages 15+) (modeled ILO estimate)`) %>%
-            group_by(Entity) %>%
-            summarize(avg = mean(female_labor), .groups = "drop") %>%
-            filter(str_detect(Entity, "^R")) %>%
-            
-            ggplot(mapping = aes(x = Entity, y = avg)) +
-            geom_point()
+      x %>%
+        rename("female_labor" = `Labor force participation rate, female (% of female population ages 15+) (modeled ILO estimate)`) %>%
+        
+        # The initial column title is to long and I can't seem to be able to put
+        # it on two lines because rename() stops working.
+        
+        group_by(Entity) %>%
+        summarize(avg = mean(female_labor), .groups = "drop") %>%
+        filter(str_detect(Entity, "^B")) %>%
+        
+        # This filters the Entity column to find all names that start with a
+        # capital B. str_detect is part of the stringr package.
+        
+        ggplot(mapping = aes(x = reorder_within(Entity, within = avg, 
+                                                by = avg), y = avg, 
+                             fill = avg)) +
+        geom_col() +
+        scale_x_reordered() +
+        theme_linedraw() +
+        theme(panel.grid.major = element_line(color = "lightgrey"),
+              panel.grid.minor = element_line(color = "lightgrey"),
+              panel.background = element_rect(fill = "white"),
+              panel.border = element_rect(color = "grey", fill = NA)) +
+        theme(axis.text.x = element_text(angle=30)) +
+        labs(title = "Women in the Workforce: By country (letter B)", 
+             x = "Countries", 
+             y = "Percentage of Women in the Workforce", 
+             caption = "\n  \n Source: Inter-Parliamentary Union",
+             fill = "Percentage") 
+      
+      # This is a combination of themes that I did in the past and really
+      # enjoyed, so decided to copy it here as well. reorder_withing and
+      # scale_x_reorder helped me reorger values n the x axis based on the
+      # values on the y axis.
 
+    })
+    
+    
+    
+    output$parliamentPlot <- renderPlot({
+      
+      post_comm_list %>%
+        rename("year" = 'year...1') %>%
+        filter(year == 2018) %>%
+        ggplot(aes(x = reorder_within(country, womenpar, womenpar), y = womenpar, color = post_comm)) +
+        geom_point() +
+        facet_wrap(~post_comm, drop = TRUE, scales = "free_x") +
+        scale_x_reordered() +
+        theme_linedraw() +
+        theme(panel.grid.major = element_line(color = "lightgrey"),
+              panel.grid.minor = element_line(color = "lightgrey"),
+              panel.background = element_rect(fill = "white"),
+              panel.border = element_rect(color = "grey", fill = NA)) +
+        scale_color_discrete(name = "Communist Status", 
+                             labels = c("Never Communist", "Post-Communist"))+
+        labs(title = "Women in Parliament: Never Communist v. Post- Communist Countries", 
+             x = "OECD Countries", 
+             y = "Percentage of Women in Parliament", 
+             caption = "\n  \n Source: CPDS") +
+        theme(axis.text.x = element_text(angle = 30))
+      
     })
 
 })
--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -11,25 +11,67 @@ library(shiny)
 library(tidyverse)
 library(shinythemes)
 library(stringr)
+library(tidytext)
 
 # Define UI for application that draws a histogram
 shinyUI(
     navbarPage(theme = shinytheme("flatly"),
-        "Women in Public Functions (Post Communist v. non-Communist countries",
+        "Women in Public Functions (Post-Communist v. Never-Communist Countries)",
                tabPanel(
                    "About",
-                   p("add my info here"), 
-                   p(a("Link to my repo:", 
+                   p("Do post-communist countries have a higher number of women
+                     in public jobs?"), 
+                   p("My name is Lavinia Teodorescu, and I am originally from a
+                     post-communist country: Romania."),
+                   p("One of the main premises of Communism is gender equality 
+                   - making jobs, education and services equal to everyone. 
+                   But living in a post-communist country, I see very few women
+                   in public jobs, and even fewer in leadership positions."),
+                   p("In this project, I will analyze the differences between 
+                   the number of women in public/ government-offered jobs in 
+                   post-communist countries v. countries that have never been 
+                   communist. What was the situation in 1980 around the globe, 
+                   and what is the situation now? Has there been any change 
+                   since these countries stopped being communist? My project 
+                   will examine these questions, as well as propose some 
+                   data-backed answers as to whether communism impacts gender
+                    equality in the jobs market"),
+                   p(a("Click Here for a link to my Repo", 
                        href = "https://github.com/teodoresculavinia/women_public_functions"))
                   
                    ),
                # You would add your content within the parentheses above.
                tabPanel(
-                   "Data",
+                   "Senate Data: 'B'",
+                   titlePanel("Percentage of Women in Senate in Countries 
+                              begining with 'B'"),
                    plotOutput("senatePlot"),
-                   p("add my info here")
-          
-               )
+                   p("This is a sample data plot that shows how widely the 
+                     percentage of women in the workforce differs around the 
+                     globe. For this specific plot, I just selected all the 
+                     countries that start with the letter 'B'. In our senario, 
+                     we can see Burundi, with over 80% of women beng in the 
+                     workforce, as opposed to Bangladesh, where just over 20 %
+                     are"),
+                  p("My dataset doesn't contain all the informations necessary 
+                    so in the future I will select only women who work in public
+                    jobs, and divide the graphs by post-communist and never
+                    communist. I am still gathering and cleaning that data.")), 
+                  
+                  tabPanel(
+                    "Parliament Data",
+                    plotOutput("parliamentPlot"),
+                    p("This dataset shows the difference in percentage of women
+                      in Parliament in different countries that are part of the 
+                      OECD. The first graph shows the countries that have never
+                      been communist, while the second one shows countries that 
+                      have been Communist in the past", 
+                    p("No post-communist country has a percentage of women of 
+                      over 35%. That is counter-intuitive to what I expected, 
+                      as one of the principle of communism is gender equality
+                      in the workforce"))) 
+                    
+
     ))
-
-
+    
+  
